957dc38e2785ddc5b4e4b60e16c4209161348753
ivan <lucovivan@ya.ru>
2024-06-17 20:31:27 +0300

    1 задание

--- Урок 14/Домашние задания/Task_1/Task_1.cpp
+++ Урок 14/Домашние задания/Task_1/Task_1.cpp
@@ -1,20 +1,80 @@
-﻿// Task_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-#include <iostream>
+vector<vector<int>> d2vector_init()
+{
+    int lines, columns; //rows, cols
+    cout << "Ведите количество строк и количество столбиков массива ";
+    cin >> lines >> columns;
+    vector<vector<int>> d2vector(lines, vector<int>(columns));
 
+    for (int i = 0; i != lines; ++i)
+    {
+        for (int j = 0; j != columns; ++j)
+        {
+            cin >> d2vector[i][j];
+        }
+    }
+
+    return d2vector;
+} 
+
+void d2vector_cout(vector<vector<int>> const& d2vector)
+{
+    for (auto const& lin : d2vector) // row : d2vector
+    {
+        for (auto col : lin) // el : row
+        {
+            cout << col << " ";
+        }
+        cout << endl;
+    }
+}
+bool IsElementInMatrix(vector<vector<int>> const& matrix, int element)
+{
+    for (const auto& lin : matrix)
+    {
+        if (find(lin.begin(), lin.end(), element) != lin.end())
+        {
+            return true;
+        }
+    }
+    return false;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "ru");
+
+    cout << "---массив A---\n";
+    vector < vector<int>> A = d2vector_init();
+    d2vector_cout(A);
+
+    cout << "---массив B---\n";
+    vector < vector<int>> B = d2vector_init();
+    d2vector_cout(A);
+
+    cout << "---массив C---\n";
+    vector < vector<int>> C = d2vector_init();
+    d2vector_cout(A);
+
+    vector<int> common_ABC;
+    for (auto const& lin : A)
+    {
+        for (int col : lin)
+        {
+            if (find(common_ABC.begin(), common_ABC.end(), col) == common_ABC.end() && IsElementInMatrix(B, col) && IsElementInMatrix(C, col))
+            {
+                common_ABC.push_back(col);
+            }
+        }
+    }
+    cout << "---общие элементы---\n";
+    for (int col : common_ABC)
+    {
+        cout << col << " ";
+    }
+    cout << "\n";
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
